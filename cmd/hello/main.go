518228f2237a42a654c0a712bce48968ac8c35b4
eldius <eldiosantos@gmail.com>
2024-01-11 20:33:13 -0300

    Creating an press ENTER to stop...

--- cmd/hello/main.go
+++ cmd/hello/main.go
@@ -1,22 +1,36 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"fmt"
 	"github.com/eldius/golang-observability-helloworld/internal/config"
 	"github.com/eldius/golang-observability-helloworld/internal/telemetry"
 	"go.opentelemetry.io/otel/attribute"
 	"math/rand"
+	"os"
 	"time"
 )
 
 func main() {
 	telemetry.InitMetrics("hello-world")
 	telemetry.InitTracer("hello-world")
+
+	go mainLoop()
+
+	fmt.Println("Press 'Enter' to continue...")
+	_, err := bufio.NewReader(os.Stdin).ReadBytes('\n')
+	if err != nil {
+		config.NewLogger(context.Background()).With("error", err).Error("ErrorReadingFromConsole")
+		panic(err)
+	}
+	os.Exit(0)
+}
+
+func mainLoop() {
 	for {
 		process()
 	}
-
 }
 
 func process() {
